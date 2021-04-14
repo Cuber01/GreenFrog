ed0998c09dbf8de50eeb0bec6b8d5329fee21c1b
Cuber01 <johnmallard631@gmail.com>
2021-04-14 21:11:18 +0200

    we have kinda working connectors now, and a todo list

--- svg_generation/svg.rb
+++ svg_generation/svg.rb
@@ -1,12 +1,45 @@
+#//TODO - 
+#
+#   ORGANIZE:
+#      - Change for to each
+#      - Add main()
+#      - Add comments for users
+#
+#
+#   UI:
+#     - Finish the basic drawing
+#     - Tag lines as levels  
+#     - Make it work with our info       
+#     - Display full info about levels
+#     - Port to html
+#     - Make it interactable and display info better
+#     - Add images
+#   
+#   DATA:
+#       - Update Progression.json
+#       - It doesnt fully work
+#       - Add a way to get images
+#       - Ignore some enemies + special tweaks
+# 
+
+
 require 'victor'
 
 
-$levels = {"seaside":[0], "crevice village":[1], "forest":[2, "bonus"], "cave":[3], 
-  "dungeon":[4, "anyway"], "castle":[5]}
+$levels = [
+  ["seaside", 0],
+  ["crevice village", 1],
+  ["forest", 2, "bonus"],
+  ["cave", 3], 
+  ["dungeon", 4, "anyway"],
+  ["castle", 5]
+]
 
 
 $nmb_of_lines = $levels.length
 $line_shift = 132
+
+$index = 0
 
 def DrawLine(svg, x, y)
 
@@ -36,15 +69,20 @@ def main()
 
       for i in 1..$nmb_of_lines do
         
-        DrawLine(svg, $line_shift*i, 200)
+        DrawLine(svg, $line_shift * i + $index, 200)
 
       end
 
-      for i in 1..$nmb_of_lines do
 
-        DrawConnector( svg, i*$line_shift+55 , 150)
-
-      end 
+      for i in 0...$nmb_of_lines do
+        puts $levels[i][2]
+        if $levels[i][2] == "bonus" then  
+          DrawConnector( svg, (i+1) * $line_shift + 55 , 150)
+        end
+        
+        #puts "#{key}"
+        
+      end
 
 
 
