59807234a151526e94e547cb15bd2dc165ab7e5e
Cuber01 <johnmallard631@gmail.com>
2021-04-14 20:10:37 +0200

    organized the code

--- svg_generation/svg.rb
+++ svg_generation/svg.rb
@@ -1,59 +1,53 @@
 require 'victor'
 
 
+$levels = {"seaside":[0], "crevice village":[1], "forest":[2, "bonus"], "cave":[3], 
+  "dungeon":[4, "anyway"], "castle":[5]}
 
-def draw_segment(svg,x,y)
-  line_shift = 132
 
+$nmb_of_lines = $levels.length
+$line_shift = 132
 
-    svg.line x1: x, y1: y+200, x2: x+230, y2: y+150, stroke: 'black'
-    svg.line x1: x, y1: y+200, x2: x+230, y2: y+150, stroke: 'black'
+def DrawLine(svg, x, y)
 
-    #svg.line x1: line_shift+50, y1: 200, x2: 230, y2: 150, stroke: 'black'
-    #svg.line x1: (line_shift+50)*2, y1: 200, x2: 230, y2: 150, stroke: 'black'
-    #svg.line x1: 270, y1: 150, x2: 300 y2: 150, stroke: 'black'
+  svg.line x1: x,
+          y1: y,
+          x2: 130+x,
+          y2: y,
+          stroke: 'black'
 
+end 
+
+def DrawConnector(svg, x, y)
+
+    # curve
+    svg.line x1: x, y1: y+50, x2: x+50, y2: y, stroke: 'black'
+
+    # straight
+    svg.line x1: x+50, y1: y, x2: x+75, y2: y, stroke: 'black'
+    
 end
-
-  
-
 
 
 def main()
 
-    levels = {"seaside":0, "crevice village":0, "forest":1, "cave":1, "dungeon":0}
-
-
-    nmb_of_lines = levels.length
-    line_shift = 132
-
-    shift = 92.5
-
-    bonus_start = 1
-
-
     svg = Victor::SVG.new width: 1000, height: 1000, style: { background: '#ddd' }
 
-    # svg.build do 
-      
-      for i in 1..nmb_of_lines do
+
+      for i in 1..$nmb_of_lines do
         
-      svg.line  x1: line_shift*i,
-         y1: 200,
-         x2: 130+line_shift*i,
-         y2: 200,
-         stroke: 'black'
+        DrawLine(svg, $line_shift*i, 200)
+
       end
 
-      for i in 1..nmb_of_lines do
+      for i in 1..$nmb_of_lines do
 
-        draw_segment( svg, i*line_shift+50 ,0 )
+        DrawConnector( svg, i*$line_shift+55 , 150)
 
       end 
 
-    #function()
-     
-    # end
+
+
 
     svg.save 'output'
 
