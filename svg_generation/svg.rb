0d077261398ce92799604e139851bc05e06fe90e
Cuber01 <johnmallard631@gmail.com>
2021-04-14 17:47:57 +0200

    working on ui

--- svg_generation/svg.rb
+++ svg_generation/svg.rb
@@ -1,19 +1,64 @@
 require 'victor'
 
 
-svg = Victor::SVG.new width: 140, height: 100, style: { background: '#ddd' }
 
-svg.build do 
-  rect x: 10, y: 10, width: 120, height: 80, rx: 10, fill: '#666'
-  
-  circle cx: 50, cy: 50, r: 30, fill: 'yellow'
-  circle cx: 58, cy: 32, r: 4, fill: 'black'
-  polygon points: %w[45,50 80,30 80,70], fill: '#666'
+def draw_segment(svg,x,y)
+  line_shift = 132
 
-  3.times do |i|
-    x = 80 + i*18
-    circle cx: x, cy: 50, r: 4, fill: 'yellow'
-  end
+
+    svg.line x1: x, y1: y+200, x2: x+230, y2: y+150, stroke: 'black'
+    svg.line x1: x, y1: y+200, x2: x+230, y2: y+150, stroke: 'black'
+
+    #svg.line x1: line_shift+50, y1: 200, x2: 230, y2: 150, stroke: 'black'
+    #svg.line x1: (line_shift+50)*2, y1: 200, x2: 230, y2: 150, stroke: 'black'
+    #svg.line x1: 270, y1: 150, x2: 300 y2: 150, stroke: 'black'
+
 end
 
-svg.save 'pacman'
+  
+
+
+
+def main()
+
+    levels = {"seaside":0, "crevice village":0, "forest":1, "cave":1, "dungeon":0}
+
+
+    nmb_of_lines = levels.length
+    line_shift = 132
+
+    shift = 92.5
+
+    bonus_start = 1
+
+
+    svg = Victor::SVG.new width: 1000, height: 1000, style: { background: '#ddd' }
+
+    # svg.build do 
+      
+      for i in 1..nmb_of_lines do
+        
+      svg.line  x1: line_shift*i,
+         y1: 200,
+         x2: 130+line_shift*i,
+         y2: 200,
+         stroke: 'black'
+      end
+
+      for i in 1..nmb_of_lines do
+
+        draw_segment( svg, i*line_shift+50 ,0 )
+
+      end 
+
+    #function()
+     
+    # end
+
+    svg.save 'output'
+
+end
+
+
+main()
+
